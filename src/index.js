03529b9cb2e572a19bbc870994b7d980f1d5cb6f
KC <link@KCs-MacBook-Air.local>
2026-07-12 22:15:24 -0700

    fix(storage): properly configure Amplify outputs for S3 uploads

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import { Amplify } from "aws-amplify";
+import outputs from "./amplify_outputs.json";
+Amplify.configure(outputs);
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
