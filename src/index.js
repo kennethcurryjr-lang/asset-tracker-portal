f718be27813ee9f46b6f26f2d56925886b35220d
KC <link@KCs-MacBook-Air.local>
2026-07-12 22:22:43 -0700

    fix(storage): reorder Amplify configuration to satisfy ESLint import rules

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import { Amplify } from "aws-amplify";
 import outputs from "./amplify_outputs.json";
-Amplify.configure(outputs);
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AuthProvider } from 'react-oidc-context';
+
+Amplify.configure(outputs);
+
 
 const cognitoConfig = {
   authority: "https://cognito-idp.us-east-2.amazonaws.com/us-east-2_CK94sKjaC",
